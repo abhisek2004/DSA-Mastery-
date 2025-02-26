5c55677bc0f03ce4608f7b21d64c79bcdade8e08
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-02-26 17:43:31 +0530

    54

---
+++ CODE/Python Code/19.py
@@ -0,0 +1,34 @@
+# # def print_trapezium(n):
+# #     for i in range(n):
+# #         print("*")
+# #         print(".")
+# #         print("*")
+
+# #     for i in range(n):
+# #         print("*")
+# #         print(".")
+# #         print("*")
+
+
+# # n = int(input())
+
+# # print_trapezium(n)
+
+# int n = Integer.parseInt(args[0]);
+# for (int i=0; i < n; i++)
+# {
+#     for (int j=0; j < n-i; j++)
+#     {
+#         System.out.print("x:"*");
+#     }
+#     for (int j=0; j <; j++)
+#     {
+#         System.out.print("x:".");
+#     }
+#     for (int j=0; j <2*i+1; j++)
+#     {
+#         System.out.print("x:"*");
+#     }
+
+# }
+

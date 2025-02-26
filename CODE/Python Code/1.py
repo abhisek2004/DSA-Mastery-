b0256cdcfc45c7bd2d714d3db46f330ae58d37d5
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-02-26 17:39:35 +0530

    37

---
+++ CODE/Python Code/1.py
@@ -0,0 +1,47 @@
+# while (True):
+#     a = int(input("Enter A value"))
+#     b = int(input("Enter B value"))
+#     n = int(input("press 1 to add\n2 to subtract "))
+#     if (n == 1):
+#         print(a+b)
+#     elif (n == 2):
+#         print(a-b)
+#     else:
+#         break
+
+# for i in range(1, 5):
+#     for j in range(5, i, -1):
+#         print("*", end=" ")
+#     print()
+
+
+# for i in range(1, 5):
+#     print("*" * (5-i))
+
+# for i in range(1, 5):
+#     print(" " *(4-i)+"*"*i)
+
+# for i in range(1, 5):
+#     print(" "*i+"*"*(5-i))
+
+# for i in range(1, 5):
+#     print(" "*(5-i)+"* "*i)
+
+
+# for i in range(1, 5):
+#     print(" "*(5-i),f"{i} "*i)
+
+# for i in range(1, 5):
+#     print(" "*(5-i),end="")
+#     print j in range (1,i+1):
+#     print(i,end=" ")
+#     print( )
+
+
+# c=0
+# for i in range(1,5):
+#     print(" "*(5-i),end="")
+#     for j in range(1,i+1):
+#     c+=1
+#     print(c,end=" ")
+#     print()

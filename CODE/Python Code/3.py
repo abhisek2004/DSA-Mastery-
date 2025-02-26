0a73648963c70d153b0f8b37f3130289f820a0e3
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-02-26 17:40:07 +0530

    40

---
+++ CODE/Python Code/3.py
@@ -0,0 +1,63 @@
+# class abc:
+#     def __init__(self):
+#         self.x = 10
+
+#     def abcd(self):
+#         print(f"Hello World{self.x}")
+
+
+# ob = abc()
+# ob.abcd()
+
+
+# class abc:
+#     def __init__(self, a):
+#         self.a = a
+#         self.b = a+a
+
+#     def abcd(self):
+#         print("Hello World")
+#         print(self.a)
+
+
+# class efg:
+#     def xyz(self):
+#         print(self.b)
+
+
+# n = int(input())
+# ob = abc()
+# ob.abcd()
+# ob1=efg()
+# ob1.xyz(ob.b)
+
+
+# class Node:
+#     def __init__(self, data):
+#         self.data = data
+#         self.next = None
+
+
+# class LinkedList:
+#     def __init__(self):
+#         self.head = None
+
+#         def insert(self, data):
+#             newNode = Node(data)
+#             if (self.head == None):
+#                 self.head = newNode
+#             else:
+#                 temp = self.head
+#                 temp.next = newNode
+
+#                 def display(self):
+#                     temp = self.head
+#                     print(temp.data)
+#                     temp = temp.next
+#                     print(temp.data)
+
+
+# ob = LinkedList()
+# ob.insert(10)
+# ob.insert(20)
+# ob.display()

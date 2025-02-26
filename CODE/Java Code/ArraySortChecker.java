a560e55b9ea0c3646a9dccc3ea46c594c5b60a7a
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-02-26 17:32:32 +0530

    15

---
+++ CODE/Java Code/ArraySortChecker.java
@@ -0,0 +1,28 @@
+// WAP to check a the array is sorted or not
+
+import java.util.Scanner;
+
+public class ArraySortChecker {
+    public static boolean isSorted(int[] array) {
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] > array[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of elements in the array:");
+        int n = scanner.nextInt();
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            array[i] = scanner.nextInt();
+        }
+        boolean sorted = isSorted(array);
+        System.out.println("Is the array sorted? " + (sorted ? "true" : "false"));
+        scanner.close();
+    }
+}

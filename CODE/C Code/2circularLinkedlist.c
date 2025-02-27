363b9df0ec30587805a087678c46741482484147
ABHISEK PANDA <117925314+abhisek2004@users.noreply.github.com>
2025-02-27 20:18:57 +0530

    27

--- CODE/C Code/2circularLinkedlist.c
+++ CODE/C Code/2circularLinkedlist.c
@@ -1,45 +1,44 @@
-#include<stdio.h>
-#include<stdlib.h>
-struct Node{
+#include <stdio.h>
+#include <stdlib.h>
+struct Node
+{
     int data;
     struct Node *next;
     struct Node *prev;
-
 };
-struct Node* createNode(){
-    struct Node *newNode = (struct Node*)malloc(sizeof(struct Node));
+struct Node *createNode()
+{
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
     printf("Enter the data: ");
-    scanf("%d",&newNode->data);
+    scanf("%d", &newNode->data);
     newNode->next = NULL;
     newNode->prev = NULL;
     return newNode;
 }
 
-int main(){
+int main()
+{
     struct Node *head = NULL;
     struct Node *tail = NULL;
     struct Node *a = NULL;
     struct Node *b = NULL;
     struct Node *c = NULL;
-    a= createNode();
-    b= createNode();
-    c= createNode();
+    a = createNode();
+    b = createNode();
+    c = createNode();
     head = a;
     a->next = b;
     b->next = c;
     b->prev = a;
-    c->prev= b;
+    c->prev = b;
     c->next = a;
     a->prev = c;
-    tail=c;
-    struct Node* temp = head;
-    while (temp!=tail){
-        printf("%d->",temp->data);
+    tail = c;
+    struct Node *temp = head;
+    while (temp != tail)
+    {
+        printf("%d->", temp->data);
         temp = temp->next;
-
     }
-    printf("%d\n",temp->data);
-
+    printf("%d\n", temp->data);
 }
-    
-
